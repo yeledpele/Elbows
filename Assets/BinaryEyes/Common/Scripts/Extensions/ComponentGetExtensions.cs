9aaaf4910491793be23db154ca7d2c002ed6d8bd
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-18 23:51:52 +0300

    added component get extensions

---
+++ Assets/BinaryEyes/Common/Scripts/Extensions/ComponentGetExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace BinaryEyes.Common.Extensions
+{
+    public static class ComponentGetExtensions
+    {
+        public static T GetNameComponent<T>(this Component source, string name)
+            where T : Component
+        {
+            var targets = source.GetComponentsInChildren<T>(true);
+            return targets.FirstOrDefault(entry => entry.name == name);
+        }
+    }
+}
