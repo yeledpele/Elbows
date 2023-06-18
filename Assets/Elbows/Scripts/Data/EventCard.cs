cbf0eb00708e44a17a15f0415bb9143f13b5e819
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-18 22:29:50 +0300

    event stuff

---
+++ Assets/Elbows/Scripts/Data/EventCard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Elbows.Data
+{
+    /// <summary>
+    /// The EventCard data container provides information about a single queue
+    /// event.
+    /// </summary>
+    public class EventCard
+        : ScriptableObject
+    {
+
+    }
+}
---
+++ Assets/Elbows/Scripts/Data/EventQueue.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Elbows.Data
+{
+    /// <summary>
+    /// The EventQueue provides information regarding a particular queue found
+    /// in an event.
+    /// </summary>
+    public class EventQueue
+        : ScriptableObject
+    {
+
+    }
+}
