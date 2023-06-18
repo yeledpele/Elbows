dd0d7e9b2c16951a27b62148aeabb9e0ee71b2af
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-18 22:51:38 +0300

    added player data container and the default-player instance

---
+++ Assets/Elbows/Scripts/Data/PlayerData.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Elbows.Data
+{
+    /// <summary>
+    /// The PlayerData container provides starting information regarding the
+    /// state of the current player.
+    /// </summary>
+    [CreateAssetMenu(menuName = "Elbows/Player Data")]
+    public class PlayerData
+        : ScriptableObject
+    {
+        [SerializeField] private string _name;
+        public string Name => _name;
+    }
+}
