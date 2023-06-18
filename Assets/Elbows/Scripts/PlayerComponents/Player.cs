9b29787e557a10e183361b8b60af7e64d730da84
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-18 22:54:53 +0300

    added player singleton and updating event panel

---
+++ Assets/Elbows/Scripts/PlayerComponents/Player.cs
@@ -0,0 +1,14 @@
+﻿using BinaryEyes.Common;
+
+namespace Elbows.PlayerComponents
+{
+    /// <summary>
+    /// The Player behaviour provides the centralized control over all player
+    /// information and operations.
+    /// </summary>
+    public class Player
+        : SingletonComponent<Player>
+    {
+
+    }
+}
