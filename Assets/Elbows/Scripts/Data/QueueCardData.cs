027cddb5e74ff62938e199e8927f400a1afb8ef7
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-18 23:54:29 +0300

    added the QueueCardData container

---
+++ Assets/Elbows/Scripts/Data/QueueCardData.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Elbows.Data
+{
+    /// <summary>
+    /// The QueueCardData data container provides information about a single queue
+    /// card (NPC or Event).
+    /// </summary>
+    [CreateAssetMenu(menuName = "Elbows/Queue Card Data")]
+    public class QueueCardData
+        : ScriptableObject
+    {
+        [SerializeField] private Sprite _backImage;
+        [SerializeField] private Sprite _frontImage;
+        public Sprite BackImage => _backImage;
+        public Sprite FrontImage => _frontImage;
+    }
+}
