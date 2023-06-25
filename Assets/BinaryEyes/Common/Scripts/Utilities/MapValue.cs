0ae058b81e6eceaf069848a304543ba9dbba05ac
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-26 00:38:12 +0300

    cleaning

---
+++ Assets/BinaryEyes/Common/Scripts/Utilities/MapValue.cs
@@ -0,0 +1,15 @@
+﻿using BinaryEyes.Common.Data;
+using BinaryEyes.Common.Extensions;
+
+namespace BinaryEyes.Common.Utilities
+{
+    public static class MapValue
+    {
+        public static float Perform(float value, Interval source, Interval target)
+        {
+            value = source.GetLocked(value);
+            var normalized = (value - source.Min) / (source.Max - source.Min);
+            return target.Min + normalized * (target.Max - target.Min);
+        }
+    }
+}
