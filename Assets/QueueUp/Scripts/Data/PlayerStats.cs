cfef06fdeb341536dd2b188fe96cf3012b6a86f5
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-27 01:08:01 +0300

    added player stats to player manager

--- Assets/QueueUp/Scripts/Data/PlayerStats.cs
+++ Assets/QueueUp/Scripts/Data/PlayerStats.cs
@@ -2,15 +2,15 @@
 
 namespace QueueUp.Data
 {
+    [CreateAssetMenu(menuName = "Queue Up/Player Stats", order = -20_000)]
     public class PlayerStats
         : ScriptableObject
     {
-
-        public int Hp;
-        public int Pat;
-        public int Ass;
-        public int Str;
-        public int Spd;
+        public int Health;
+        public int Patience;
+        public int Assertiveness;
+        public int Strength;
+        public int Speed;
 
         public Sprite PlayerImage; // in case we want to add an image somewhere
     }
--- Assets/QueueUp/Scripts/PlayerManager.cs
+++ Assets/QueueUp/Scripts/PlayerManager.cs
@@ -1,10 +1,45 @@
 ﻿using BinaryEyes.Common;
+using BinaryEyes.Common.Attributes;
+using BinaryEyes.Common.Data;
+using QueueUp.Data;
+using UnityEngine;
+using Event = BinaryEyes.Common.Data.Event;
 
 namespace QueueUp
 {
     public class PlayerManager
         : SingletonComponent<PlayerManager>
     {
+        [SerializeField] [ReadOnlyField] private int _health;
+        [SerializeField] [ReadOnlyField] private int _patience;
+        [SerializeField] [ReadOnlyField] private int _assertiveness;
+        [SerializeField] [ReadOnlyField] private int _strength;
+        [SerializeField] [ReadOnlyField] private int _speed;
+        [SerializeField] private PlayerStats _stats;
+        [SerializeField] private Event _healthChanged;
+        [SerializeField] private Event _patienceChanged;
+        [SerializeField] private Event _assertivenessChanged;
+        [SerializeField] private Event _strengthChanged;
+        [SerializeField] private Event _speedChanged;
+        public int Health => _health;
+        public int Patience => _patience;
+        public int Assertiveness => _assertiveness;
+        public int Strength => _strength;
+        public int Speed => _speed;
+        public IEvent HealthChanged => _healthChanged;
+        public IEvent PatienceChanged => _patienceChanged;
+        public IEvent AssertivenessChanged => _assertivenessChanged;
+        public IEvent StrengthChanged => _strengthChanged;
+        public IEvent SpeedChanged => _speedChanged;
 
+        protected override void Awake()
+        {
+            _health = _stats.Health;
+            _patience = _stats.Patience;
+            _assertiveness = _stats.Assertiveness;
+            _strength = _stats.Strength;
+            _speed = _stats.Speed;
+            base.Awake();
+        }
     }
 }
