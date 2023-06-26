e6cc96bf3d05e01d89c4d390d665848885a2ef22
yeledpele <common.typo@gmail.com>
2023-06-27 00:43:05 +0300

    scriptable obj for NPC and Event

--- Assets/QueueUp/Scripts/Data/CardsData/CharacterCardData.cs
+++ Assets/QueueUp/Scripts/Data/CardsData/CharacterCardData.cs
@@ -1,6 +1,23 @@
-﻿namespace QueueUp.Data.CardsData
+﻿using UnityEngine;
+
+namespace QueueUp.Data.CardsData
 {
-    internal class CharacterCardData
+    [CreateAssetMenu(fileName = "New Character Card Data", menuName = "Card Data/Character Card Data")]
+    public class CharacterCardData : ScriptableObject
     {
+        public int Hp;
+        public int Pat;
+        public int Ass;
+        public int Str;
+        public int Spd;
+
+
+        // Assuming the card has a color property
+
+        // public Color cardColor;
+
+        // Additional properties specific to the character card, if any
+
+        // Add any other methods or variables as needed
     }
 }
