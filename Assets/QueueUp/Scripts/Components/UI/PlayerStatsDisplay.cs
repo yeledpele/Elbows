7fa7101bc2e328b14a23e882dda5f9f760d573e2
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-29 23:35:53 +0300

    added player stats display

---
+++ Assets/QueueUp/Scripts/Components/UI/PlayerStatsDisplay.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace QueueUp.Components.UI
+{
+    public class PlayerStatsDisplay
+        : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _healthDisplay;
+        [SerializeField] private TextMeshProUGUI _patienceDisplay;
+        [SerializeField] private TextMeshProUGUI _assertivenessDisplay;
+
+        private void Start()
+        {
+            _healthDisplay.text = PlayerManager.Instance.Health.ToString();
+            _patienceDisplay.text = PlayerManager.Instance.Patience.ToString();
+            _assertivenessDisplay.text = PlayerManager.Instance.Assertiveness.ToString();
+            PlayerManager.Instance.HealthChanged.AddListener(() =>
+                _healthDisplay.text = PlayerManager.Instance.Health.ToString());
+
+            PlayerManager.Instance.PatienceChanged.AddListener(() =>
+                _patienceDisplay.text = PlayerManager.Instance.Patience.ToString());
+
+            PlayerManager.Instance.AssertivenessChanged.AddListener(() =>
+                _assertivenessDisplay.text = PlayerManager.Instance.Assertiveness.ToString());
+        }
+    }
+}
