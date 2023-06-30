728cfad99c44b9f58c31e876be991dc2b7003adf
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-30 10:22:58 +0300

    working on messages

--- Assets/QueueUp/Scripts/CombatResolver.cs
+++ Assets/QueueUp/Scripts/CombatResolver.cs
@@ -1,26 +1,46 @@
-﻿using System.Linq;
+﻿using System.Collections;
 using BinaryEyes.Common;
 using BinaryEyes.Common.Extensions;
+using QueueUp.Components.UI;
 using QueueUp.Data.CardsData;
+using UnityEngine;
+// ReSharper disable StringLiteralTypo
 
 namespace QueueUp
 {
     public class CombatResolver
         : SingletonComponent<CombatResolver>
     {
+        [SerializeField] private MessagePanel _messagePanel;
+
         public void ResolvePlayerCombat(CharacterCardData npc)
         {
             this.LogMessage($"ResolvingPlayerCombat: npc={npc.DisplayName}");
+            StartCoroutine(PerformPlayerCombat(npc));
+        }
+
+        private IEnumerator PerformPlayerCombat(CharacterCardData npc)
+        {
             var player = PlayerManager.Instance;
 
             if (npc.Speed >= player.Speed)
             {
-                this.LogMessage($"{npc.DisplayName.Reverse()} attack player for {npc.Strength} damage");
+                this.LogMessage($"{npc.name} attack player for {npc.Strength} damage");
+                yield return _messagePanel.StartShow($"{npc.DisplayName} הכניס/ה לך מכות");
+                yield return new WaitForSeconds(0.5f);
             }
             else
             {
-                this.LogMessage($"Player attacks {npc.DisplayName.Reverse()} for {player.Strength} damage");
+                this.LogMessage($"Player attacks {npc.name} for {player.Strength} damage");
+                yield return _messagePanel.StartShow($"פצצת את {npc.DisplayName} במכות");
             }
+        }
+
+        private IEnumerator ShowMessage(string message)
+        {
+            yield return _messagePanel.StartShow(message);
+            yield return new WaitForSeconds(0.5f);
+            yield return _messagePanel.StartHide();
         }
     }
 }
