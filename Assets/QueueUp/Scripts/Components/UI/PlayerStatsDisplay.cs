30695e39f48a30dd3df3503771abbc45c0818bbf
AmirBarak <binary.eyes.dev@gmail.com>
2023-06-30 11:18:41 +0300

    added more player stats displays

--- Assets/QueueUp/Scripts/Components/UI/PlayerStatsDisplay.cs
+++ Assets/QueueUp/Scripts/Components/UI/PlayerStatsDisplay.cs
@@ -9,12 +9,17 @@ namespace QueueUp.Components.UI
         [SerializeField] private TextMeshProUGUI _healthDisplay;
         [SerializeField] private TextMeshProUGUI _patienceDisplay;
         [SerializeField] private TextMeshProUGUI _assertivenessDisplay;
+        [SerializeField] private TextMeshProUGUI _strengthDisplay;
+        [SerializeField] private TextMeshProUGUI _speedDisplay;
 
         private void Start()
         {
             _healthDisplay.text = PlayerManager.Instance.Health.ToString();
             _patienceDisplay.text = PlayerManager.Instance.Patience.ToString();
             _assertivenessDisplay.text = PlayerManager.Instance.Assertiveness.ToString();
+            _strengthDisplay.text = PlayerManager.Instance.Strength.ToString();
+            _speedDisplay.text = PlayerManager.Instance.Speed.ToString();
+
             PlayerManager.Instance.HealthChanged.AddListener(() =>
                 _healthDisplay.text = PlayerManager.Instance.Health.ToString());
 
@@ -23,6 +28,12 @@ namespace QueueUp.Components.UI
 
             PlayerManager.Instance.AssertivenessChanged.AddListener(() =>
                 _assertivenessDisplay.text = PlayerManager.Instance.Assertiveness.ToString());
+
+            PlayerManager.Instance.StrengthChanged.AddListener(() =>
+                _strengthDisplay.text = PlayerManager.Instance.Strength.ToString());
+
+            PlayerManager.Instance.SpeedChanged.AddListener(() =>
+                _speedDisplay.text = PlayerManager.Instance.Speed.ToString());
         }
     }
 }
